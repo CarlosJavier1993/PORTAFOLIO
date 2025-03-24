dfba7587938e0cdf32c5cbb42fc359b451356d9c
Carlos Ramos <carlos.ramos-c@uniminuto.edu.co>
2025-03-24 09:36:49 -0500

    Modificación en build_files.sh para asegurar compatibilidad con Python

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+#!/bin/bash
+
+# Instala pip si no está disponible
+python -m ensurepip --upgrade || python -m ensurepip --upgrade
+
+# Instala las dependencias desde requirements.txt
+python -m pip install -r requirements.txt || python -m pip install -r requirements.txt
+
+# Recopila archivos estáticos
+python manage.py collectstatic --noinput || python manage.py collectstatic --noinput
